811a1aed5e013f288b5b1b0f31bd1c64c124e376
chopperman <529953602@qq.com>
2019-04-30 16:51:56 +0800

    modify

--- src/main/resources/soraka.sql
+++ src/main/resources/soraka.sql
@@ -1,95 +1,206 @@
 /*
- Navicat Premium Data Transfer
-
- Source Server         : this.pc
- Source Server Type    : MySQL
- Source Server Version : 50726
- Source Host           : localhost:3306
- Source Schema         : soraka
-
- Target Server Type    : MySQL
- Target Server Version : 50726
- File Encoding         : 65001
-
- Date: 29/04/2019 21:24:42
+SQLyog Ultimate v12.3.1 (64 bit)
+MySQL - 5.7.24 : Database - soraka
+*********************************************************************
 */
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
 
--- ----------------------------
--- Table structure for basic_table
--- ----------------------------
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE = '' */;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0 */;
+/*!40101 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES = @@SQL_NOTES, SQL_NOTES = 0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS */`soraka` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `soraka`;
+
+/*Table structure for table `basic_table` */
+
 DROP TABLE IF EXISTS `basic_table`;
-CREATE TABLE `basic_table`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `gmt_create` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
-  `gmt_modified` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Table structure for sys_authorities
--- ----------------------------
+CREATE TABLE `basic_table` (
+  `id`           int(11)  NOT NULL AUTO_INCREMENT
+  COMMENT '主键',
+  `gmt_create`   datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `gmt_modified` datetime          DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
+  ROW_FORMAT = DYNAMIC;
+
+/*Data for the table `basic_table` */
+
+/*Table structure for table `order_info` */
+
+DROP TABLE IF EXISTS `order_info`;
+
+CREATE TABLE `order_info` (
+  `order_id`      int(11)     NOT NULL AUTO_INCREMENT
+  COMMENT '主键',
+  `gmt_create`    datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `gmt_pay`       datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id`       varchar(50) NOT NULL
+  COMMENT '用户id',
+  `parking_name`  varchar(32) NOT NULL DEFAULT ''
+  COMMENT '停车场名',
+  `parking_id`    varchar(50) NOT NULL
+  COMMENT '停车场id',
+  `parking_place` tinyint(1)  NOT NULL
+  COMMENT '车位',
+  `price`         varchar(32)          DEFAULT NULL
+  COMMENT '价格',
+  `state`         varchar(2)           DEFAULT NULL
+  COMMENT '状态',
+  PRIMARY KEY (`order_id`) USING BTREE
+)
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 7
+  DEFAULT CHARSET = utf8
+  ROW_FORMAT = DYNAMIC
+  COMMENT = '订单信息表';
+
+/*Data for the table `order_info` */
+
+/*Table structure for table `sys_authorities` */
+
 DROP TABLE IF EXISTS `sys_authorities`;
-CREATE TABLE `sys_authorities`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `gmt_create` datetime(6) NOT NULL,
-  `gmt_modified` datetime(6) NULL DEFAULT NULL,
-  `user_id` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户id',
-  `authority` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户角色（默认ROLE_USER）',
+
+CREATE TABLE `sys_authorities` (
+  `id`           int(11)     NOT NULL AUTO_INCREMENT
+  COMMENT '主键',
+  `gmt_create`   datetime(6) NOT NULL,
+  `gmt_modified` datetime(6)          DEFAULT NULL,
+  `user_id`      varchar(50) NOT NULL
+  COMMENT '用户id',
+  `authority`    varchar(50) NOT NULL
+  COMMENT '用户角色（默认ROLE_USER）',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+)
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 2
+  DEFAULT CHARSET = utf8
+  ROW_FORMAT = DYNAMIC;
 
--- ----------------------------
--- Records of sys_authorities
--- ----------------------------
-INSERT INTO `sys_authorities` VALUES (1, '2019-02-28 22:22:20.000000', '2019-02-28 22:22:18.000000', 'ms183KVBTWajhpeqUX9+KA', 'ROLE_USER');
+/*Data for the table `sys_authorities` */
 
--- ----------------------------
--- Table structure for sys_user
--- ----------------------------
+insert into `sys_authorities` (`id`, `gmt_create`, `gmt_modified`, `user_id`, `authority`)
+values (1, '2019-02-22 09:08:22.000000', '2019-02-22 09:08:22.000000', 'qpDdvw5vR12iFpXJAH+QBQ', 'ROLE_USER');
+
+/*Table structure for table `sys_user` */
+
 DROP TABLE IF EXISTS `sys_user`;
-CREATE TABLE `sys_user`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `gmt_create` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
-  `gmt_modified` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
-  `user_id` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户id',
-  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户密码',
-  `enabled` tinyint(1) NOT NULL COMMENT '是否启用(0未启用1启用）',
+
+CREATE TABLE `sys_user` (
+  `id`           int(11)      NOT NULL AUTO_INCREMENT
+  COMMENT '主键',
+  `gmt_create`   datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `gmt_modified` datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id`      varchar(50)  NOT NULL
+  COMMENT '用户id',
+  `password`     varchar(255) NOT NULL
+  COMMENT '用户密码',
+  `enabled`      tinyint(1)   NOT NULL
+  COMMENT '是否启用(0未启用1启用）',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+)
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 8
+  DEFAULT CHARSET = utf8
+  ROW_FORMAT = DYNAMIC;
 
--- ----------------------------
--- Records of sys_user
--- ----------------------------
-INSERT INTO `sys_user` VALUES (6, '2019-02-21 22:50:49', '2019-02-21 22:50:49', 'ms183KVBTWajhpeqUX9+KA', '$2a$10$wGwJT6OmAwUqOiXSl6IDCe3YS/zFx1XQrR6KK8SmPFueaSc1OdXcm', 1);
+/*Data for the table `sys_user` */
 
--- ----------------------------
--- Table structure for sys_user_info
--- ----------------------------
+insert into `sys_user` (`id`, `gmt_create`, `gmt_modified`, `user_id`, `password`, `enabled`)
+values (7,
+        '2019-02-22 09:08:22',
+        '2019-02-22 09:08:22',
+        'qpDdvw5vR12iFpXJAH+QBQ',
+        '$2a$10$X7QFJYhIZPNhIkmMAfAq/eZA99F0BSQ.8gCWe2hmqPkgoV.Z2D0ya',
+        1);
+
+/*Table structure for table `sys_user_info` */
+
 DROP TABLE IF EXISTS `sys_user_info`;
-CREATE TABLE `sys_user_info`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `gmt_create` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
-  `gmt_modified` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
-  `user_id` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户id',
-  `user_name` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '用户名',
-  `gender` tinyint(1) NOT NULL COMMENT '性别(0女1男)',
-  `age` int(3) NULL DEFAULT NULL COMMENT '年龄',
-  `birthday` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '生日',
-  `address` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '地址',
-  `user_type` varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户类型',
-  `real_name` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '真实姓名',
-  `qq` varchar(14) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'QQ',
-  `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '电子邮箱',
-  `tel` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '联系电话',
+
+CREATE TABLE `sys_user_info` (
+  `id`            int(11)     NOT NULL AUTO_INCREMENT
+  COMMENT '主键',
+  `gmt_create`    datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `gmt_modified`  datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id`       varchar(50) NOT NULL
+  COMMENT '用户id',
+  `user_name`     varchar(32) NOT NULL DEFAULT ''
+  COMMENT '用户名',
+  `gender`        tinyint(1)  NOT NULL
+  COMMENT '性别(0女1男)',
+  `age`           int(3)               DEFAULT NULL
+  COMMENT '年龄',
+  `birthday`      datetime             DEFAULT CURRENT_TIMESTAMP
+  COMMENT '生日',
+  `address`       varchar(100)         DEFAULT NULL
+  COMMENT '地址',
+  `user_type`     varchar(2)           DEFAULT NULL
+  COMMENT '用户类型',
+  `real_name`     varchar(32)          DEFAULT NULL
+  COMMENT '真实姓名',
+  `qq`            varchar(14)          DEFAULT NULL
+  COMMENT 'QQ',
+  `email`         varchar(100)         DEFAULT NULL
+  COMMENT '电子邮箱',
+  `tel`           varchar(255)         DEFAULT NULL
+  COMMENT '联系电话',
+  `license_plate` varchar(100)         DEFAULT NULL
+  COMMENT '车牌',
+  `vehicle_type`  varchar(100)         DEFAULT NULL
+  COMMENT '车型',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户信息表' ROW_FORMAT = Dynamic;
+)
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 8
+  DEFAULT CHARSET = utf8
+  ROW_FORMAT = DYNAMIC
+  COMMENT = '用户信息表';
 
--- ----------------------------
--- Records of sys_user_info
--- ----------------------------
-INSERT INTO `sys_user_info` VALUES (6, '2019-02-21 22:50:49', '2019-02-21 22:50:49', 'ms183KVBTWajhpeqUX9+KA', 'admin', 1, 11, '2019-02-21 22:50:49', '1', '1', '1', '1', '1', '1');
+/*Data for the table `sys_user_info` */
 
-SET FOREIGN_KEY_CHECKS = 1;
+insert into `sys_user_info` (`id`,
+                             `gmt_create`,
+                             `gmt_modified`,
+                             `user_id`,
+                             `user_name`,
+                             `gender`,
+                             `age`,
+                             `birthday`,
+                             `address`,
+                             `user_type`,
+                             `real_name`,
+                             `qq`,
+                             `email`,
+                             `tel`,
+                             `license_plate`,
+                             `vehicle_type`)
+values (7,
+        '2019-02-22 09:08:22',
+        '2019-02-22 09:08:22',
+        'qpDdvw5vR12iFpXJAH+QBQ',
+        'admin',
+        1,
+        1,
+        '2019-02-22 09:08:22',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        NULL,
+        NULL);
+
+/*!40101 SET SQL_MODE = @OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES = @OLD_SQL_NOTES */;
